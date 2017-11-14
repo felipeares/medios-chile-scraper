e8db29dc7e38794768b9af03903e16070177fc67
felipeares <felipeares@gmail.com>
2017-11-14 12:53:38 -0300

    Point public dir to /

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-var path    = require("path");
+var path = require("path");
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname+'/visualization/index.html'));
 });
 
-app.use(express.static('visualization/public'));
+app.use(express.static('visualization'));
 
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
